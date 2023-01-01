a27d21b6b579e38edb011204036fe71e52e812bc
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-01 17:28:35 +0000

    implemented Bit Counting kata
    implemented Bit Counting kata and its test file in javascript

---
+++ Bit_Counting.test.js
@@ -0,0 +1,13 @@
+import {countBits} from "./Bit_Counting";
+
+const { assert } = require("chai");
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(countBits(0), 0);
+    assert.strictEqual(countBits(4), 1);
+    assert.strictEqual(countBits(7), 3);
+    assert.strictEqual(countBits(9), 2);
+    assert.strictEqual(countBits(10), 2);
+    })
+  });
