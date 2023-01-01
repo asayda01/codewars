dd9c13eed296a1efd0b20ec1f90e5b53b073e37a
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-01 15:59:10 +0000

    added some new katas and their tests
    added some new katas
    
    - implemented descending order kata
    
    - implemented duplicate encode kata
    
    - implemented descending order test
    
    - implemented duplicate encode test

---
+++ duplicate_encode.test.js
@@ -0,0 +1,17 @@
+export {duplicateEncode} from "./Duplicate_Encoder";
+
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Duplicate Encoder", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(duplicateEncode("din"),"(((");
+    assert.strictEqual(duplicateEncode("recede"),"()()()");
+    assert.strictEqual(duplicateEncode("Success"),")())())","should ignore case");
+    assert.strictEqual(duplicateEncode("(( @"),"))((");
+  });
+});
+
