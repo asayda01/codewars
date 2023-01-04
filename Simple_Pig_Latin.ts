ff7268c5d8075ae98f89a5d58d1245d44feb9a22
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-04 12:25:15 +0000

    added the Simple Pig Latin kata in TypeScript
    added the Simple Pig Latin kata in TypeScript.

---
+++ Simple_Pig_Latin.ts
@@ -0,0 +1,21 @@
+/*
+
+5 kyu 
+
+Simple Pig Latin
+
+Move the first letter of each word to the end of it, 
+then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+*/
+
+
+export const pigIt = (a : string) : string =>  {
+    return a.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3");
+  };
