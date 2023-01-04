3ce7bcd73a1f3fa5259e5e40c72c3d299186d424
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-04 11:13:08 +0000

    added the Split String kata
    added the Split String kata and its primitive solution

---
+++ Split_Strings.test.js
@@ -0,0 +1,12 @@
+import {solution} from "./Split_Strings";
+
+
+const { assert } = require('chai');
+
+describe("Split Strings", () => {
+  it("Basic tests", () => {
+    assert.deepEqual(solution("abcdef"), ["ab", "cd", "ef"]);
+    assert.deepEqual(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+    assert.deepEqual(solution(""), []);
+  });
+});
