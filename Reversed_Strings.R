e99150174d24d7b96b3615044ce0976051dc09f9
asayda01 <ahmetcemsaydam@gmail.com>
2023-04-10 15:34:37 +0100

    added a solution to the Reversed String Kata
    added a solution to the Reversed String Kata in R .
    
    added a solution to the Reversed String Kata in R .

---
+++ Reversed_Strings.R
@@ -0,0 +1,19 @@
+"""
+
+8 kyu
+Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+
+"""
+
+solution <- function(s){
+  
+  paste(rev(unlist(strsplit(s,NULL))),collapse="")
+  
+}
