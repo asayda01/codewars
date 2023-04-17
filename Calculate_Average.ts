35cf57ddd40877740ad82f9a19594e9ff63357e3
asayda01 <ahmetcemsaydam@gmail.com>
2023-04-17 10:27:28 +0100

    added the Calculate Average kata and its solution in TypeScript
    added the Calculate Average kata and its solution in TypeScript .
    
    added the Calculate Average kata and its solution in TypeScript .

---
+++ Calculate_Average.ts
@@ -0,0 +1,10 @@
+/*
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+*/
+export function findAverage(array: number[]): number {
+    return !array.length ? 0 : array.reduce((a, b) => a + b, 0) / array.length;
+}
