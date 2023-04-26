7f09786455a6cb6f3bbf79d0fd9045715fe0bf5e
asayda01 <ahmetcemsaydam@gmail.com>
2023-04-26 11:31:27 +0100

    added The A Needle In The Haystack kata and its solution
    added The A Needle In The Haystack kata and its solution in Python .
    
    added The A Needle In The Haystack kata and its solution in Python .

---
+++ A_Needle_In_The_Haystack.py
@@ -0,0 +1,34 @@
+"""
+
+8 yku
+A Needle in the Haystack
+
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk 
+but containing one "needle"
+
+After your function finds the needle it should return a message 
+(as a string) that says:
+
+"found the needle at position "
+ plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] -->
+ "found the needle at position 5" 
+
+
+"""
+
+def find_needle(haystack):
+	return 'found the needle at position {}'.format(haystack.index('needle'))
+
+
+"""
+def find_needle(haystack):
+    for i in range (len(haystack)):
+        if haystack[i] == "needle":
+            return "found the needle at position " + str(i)
+"""
