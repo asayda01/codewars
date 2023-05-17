afbd9f4cd63291a251188ffd2a61b66135d4cf61
asayda01 <ahmetcemsaydam@gmail.com>
2023-05-17 10:41:38 +0100

    added the Reversed Strings Kata and its solution
    added the Reversed Strings Kata and its solution in TypeScript .
    
    added the Reversed Strings Kata and its solution in TypeScript .

---
+++ Reversed_Strings.ts
@@ -0,0 +1,17 @@
+/*
+
+8 kyu
+Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+
+
+*/
+
+export function solution(str: string): string {
+    return str.split("").reverse().join("");
+};
