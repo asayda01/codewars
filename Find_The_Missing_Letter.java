b9614f1005d894ec1b3dbc41e762c37c98078f95
asayda01 <ahmetcemsaydam@gmail.com>
2023-09-21 11:01:47 +0100

    added the Find The Missing Letter Kata
    added the Find The Missing Letter Kata and its solution in Java .
    
    added the Find The Missing Letter Kata and its solution in Java .

---
+++ Find_The_Missing_Letter.java
@@ -0,0 +1,10 @@
+public class Kata {
+
+    public static char findMissingLetter(char[] array) {
+      for (int i=1; i<array.length; i++) {
+        if (array[i]-array[i-1]!=1) return (char)(array[i]-1);
+      }
+      return '?';
+    }
+    
+}
